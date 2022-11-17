1ce2a68df1629133a44aa3686cd872a95ef5ccd7
Luiza Oliveira <luiza.oliv391@gmail.com>
2022-11-17 13:35:15 -0300

    Classes Seller e Vet
    Criado as classes Seller e Vet

---
+++ PetShop/src/Class/Seller.java
@@ -0,0 +1,19 @@
+package Class;
+
+
+import Interface.InterfaceVendedor;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/*creating the seller class and making it inherit data from the user class*/
+
+public class Seller extends User implements InterfaceSeller  {
+
+   /*creating the seller constructor*/ 
+
+   public Seller(PetInventory petInventory, String name, String user, String password, double salary) {
+        super(petInventory, name, TypeRole.SELLER, user, password, salary);
+    }
+}
