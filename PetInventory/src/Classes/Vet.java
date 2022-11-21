e2452a28452c359736f20a7fa9b7e2a63ec88cea
Luiza Oliveira <luiza.oliv391@gmail.com>
2022-11-21 15:18:34 -0300

    Alteração linha 14

--- PetInventory/src/Classes/Vet.java
+++ PetInventory/src/Classes/Vet.java
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;
 public class Vet extends User implements InterfaceVet {
 
     /*creating the vet constructor*/ 
-    public Vet(PetInventory petInventory, String name,  String usuario, String password, double salary) {
+    public Vet(PetInventory petInventory, String name,  String user, String password, double salary) {
         super(petInventory, name, TypeRole.VET, user, password, salary);
     }
 }
